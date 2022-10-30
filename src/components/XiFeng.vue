9017b2ad5a4dc2db3ab463b8a42d1c2284be77da
xsaf <1767406957@qq.com>
2022-10-30 17:39:13 +0800

    封装 Topnav 组件

--- src/components/XiFeng.vue
+++
@@ -1,3 +0,0 @@
-<template>
-  <div>这是我的第一个组件</div>
-</template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,5 @@
 <template>
-  <div class="topnav">
-    <div class="logo">LOGO</div>
-    <ul class="menu">
-      <li>菜单1</li>
-      <li>菜单2</li>
-    </ul>
-  </div>
+  <Topnav />
   <div class="banner">
     <h1>UI</h1>
     <h2>UI框架</h2>
@@ -16,31 +10,13 @@
   </div>
 </template>
 
-<script>
+<script lang="ts">
+import Topnav from '../components/Topnav.vue';
 export default {
-
+  components: { Topnav }
 }
 </script>
 
 <style lang="scss" scoped>
-.topnav {
-  background: pink;
-  display: flex;
-  padding: 16px;
 
-  >.logo {
-    max-width: 6em;
-    margin-right: auto;
-  }
-
-  >.menu {
-    display: flex;
-    white-space: nowrap;
-    flex-wrap: nowrap;
-
-    >li {
-      margin: 0 1em;
-    }
-  }
-}
 </style>
