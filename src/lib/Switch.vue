c503e0b35cbe7adbc2458239ec5b53a495ba7658
xsaf <1767406957@qq.com>
2022-11-05 16:21:59 +0800

    :class等于true也没有class

---
+++ src/lib/Switch.vue
@@ -0,0 +1,45 @@
+<template>
+<button @click="toggle" :class="{checked: x}"> <span> </span> </button>
+</template>
+
+<script lang="ts">
+import { ref } from '@vue/reactivity'
+export default {
+  setup(){
+    const x = ref(false)
+    const toggle =() =>{
+      console.log(x.value);
+      x.value = !x.value
+    } 
+    return {x,toggle}
+  } 
+}
+</script>
+
+<style lang="scss" scoped>
+$h:22px;
+  $h2:$h - 4px;
+  button{
+    height: $h;
+    width: $h * 2;
+    border: none;
+    background: grey;
+    border-radius: $h/2;
+    position: relative;
+  }
+  span{
+    position: absolute;
+    top: 2px;
+    left: 2px;
+    height: $h2;
+    width: $h2;
+    background: white;
+    border-radius: $h2 /2;
+  }
+  button.checked {
+    background: blue;
+  }
+  button.checked > span{
+    left: calc(100% - 2px - #{$h2});
+  }
+</style>
