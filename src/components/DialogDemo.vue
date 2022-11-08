d2dcdf86a249d7230fba4108f5e1e5d7182c3781
xsaf <1767406957@qq.com>
2022-11-08 16:52:06 +0800

    创建Dialog组件

---
+++ src/components/DialogDemo.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>Dialog 示例</div>
+  <h1>示例1</h1>
+  <Button @click="toggle">toggle</Button>
+  <Dialog :visible="x"></Dialog>
+</template>
+
+<script>
+import Dialog from '../lib/Dialog.vue';
+import Button from '../lib/Button.vue';
+import { ref } from 'vue';
+export default {
+  components:{
+    Dialog, Button
+  },
+  setup(){
+    const x = ref(false)
+    const toggle = ()=>{
+      x.value=!x.value
+    }
+    return {x,toggle}
+  }
+}
+</script>
+
+<style>
+
+</style>
